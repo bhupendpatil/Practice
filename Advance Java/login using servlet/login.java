f2e1ce6e08b227f634b4aa574fcc058e8e4ffed1
Bhupend Patil <bhupendpatil@gmail.com>
2018-01-31 15:18:36 +0530

    removed unwanted code :clapper:

--- Advance Java/login using servlet/login.java
+++ Advance Java/login using servlet/login.java
@@ -1,16 +1,13 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 
 @WebServlet(urlPatterns = {"/login"})
 public class login extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
