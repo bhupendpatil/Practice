ff3d281ccd2f915b89fed1e14f3641fbe9dd40a9
Bhupend Patil <bhupendpatil@gmail.com>
2018-03-15 20:11:28 +0530

    Required field validator PHP

---
+++ WEB/PHP/requiredFieldValidation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Required Field Validation</title>
+</head>
+<body>
+<?php 
+
+	$nameErr = "";
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["name"])){
+			$nameErr = "Name is required";
+		}
+	}
+		
+	?>
+	<form method = "POST" action="">
+		Name : <input type="text" name="name"><?php echo $nameErr; ?><br/>
+
+		<input type="submit" name="submit" value="submit">
+	</form>
+</body>
+</html>
