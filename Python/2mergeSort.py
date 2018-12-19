6831bba39bf634ca86ae2cb1579cc37056c5715d
Bhupend Patil <bhupendpatil@gmail.com>
2018-12-19 15:29:43 +0530

    fixed minor typo bug and indentations according to IDE :hocho:

--- Python/2mergeSort.py
+++ Python/2mergeSort.py
@@ -7,36 +7,36 @@ Compare the time and memory complexity.
 import time
 
 def mergeSort(a):
-	if len(a)>1:
-		mid = len(a)//2
-		left = a[:mid]
-		right = a[mid:]
-
-		mergeSort(left)
-		mergeSort(right)
-
-		i = 0
-		j = 0
-		k = 0
-		while i<len(left) and j<len(right):
-			if left[i] < right[i]:
-				a[k] = left[i]
-				i += 1
-			else:
-				a[k] = right[j]
-				j += 1
-			k += 1
-
-		while i < len(left):
-			a[k]=left[i]
-			i += 1
-			k += 1
-
-		while j < len(right):
-			a[k]=right[j]
-			j += 1
-			k += 1
-
+    if len(a)>1:
+        mid = len(a)//2
+        left = a[:mid]
+        right = a[mid:]
+	
+        mergeSort(left)
+        mergeSort(right)
+	
+        i = 0
+        j = 0
+        k = 0
+        while i<len(left) and j<len(right):
+            if left[i] < right[j]:
+                a[k] = left[i]
+                i += 1
+            else:
+                a[k] = right[j]
+                j += 1
+            k += 1
+                
+        while i < len(left):
+            a[k]=left[i]
+            i += 1
+            k += 1
+            
+        while j < len(right):
+            a[k]=right[j]
+            j += 1
+            k += 1
+            
 
 toSort = [10,5,88,5,3]
 
@@ -57,6 +57,6 @@ Output
 Unsorted:
 [10, 5, 88, 5, 3]
 Sorted:
-[3, 5, 88, 5, 10]
-Running time = 0.00012421607971191406
+[3, 5, 5, 10, 88]
+Running time = 0.035976409912109375
 """
