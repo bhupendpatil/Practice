973019d0cbc57ba9353375930880ebeca7338bc8
Bhupend Patil <bhupendpatil@gmail.com>
2019-01-20 15:37:10 +0530

    deleting

--- Python/Robotics/makeSquare.py
+++
@@ -1,7 +0,0 @@
-# Write a program to do a square 
-# using while loop,
-# doing steps with for loop,
-# to change directions based on condition,
-# controlling motor speed using switch case
-
-# Updating soon..
