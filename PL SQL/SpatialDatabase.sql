581e4755550cd5f06e89a8d873118b5998a54572
Bhupend Patil <bhupendpatil@gmail.com>
2019-01-27 01:40:37 +0530

    :fork_and_knife:

--- PL SQL/SpatialDatabase.sql
+++ PL SQL/SpatialDatabase.sql
@@ -43,30 +43,30 @@ insert into user_SDO_GEOM_METADATA values
 		MDSYS.SDO_DIM_ELEMENT('Y',0,20,0.005)),
 		NULL);
 
-create index cola_spatial_idx
-		on cola_mrp(shape)
-			Indextype Is mdsys.spatial_index;
+create index cola_spatial_idx on cola_mrp(shape) Indextype Is mdsys.spatial_index;
 
 -- Queries
 
 -- a) Find the topological intersection of 2 geometries.
 select SDO_GEOM.SDO_INTERSECTION(c_a.shape,c_c.shape,0.005)
-from cola_mrp c_a,cola_mrp c_c
-where c_a.name='cola_a' AND c_c.name='cola_c';
+	FROM cola_mrp c_a,cola_mrp c_c
+	WHERE c_a.name='cola_a' AND c_c.name='cola_c';
 
 -- b) Find whether 2 geometric figures are equivalent to each other.
 SELECT SDO_GEOM.RELATE(c_c.shape,'EQUAL',c_a.shape,0.005)
-FROM cola_mrp c_c,cola_mrp c_a
-WHERE c_c.name='cola_c' AND c_a.name='cola_a';
+	FROM cola_mrp c_c,cola_mrp c_a
+	WHERE c_c.name='cola_c' AND c_a.name='cola_a';
 
 -- c) Find the areas of all different location.
-SELECT name,SDO_GEOM.SDO_AREA(shape,0.005) from cola_mrp;
+SELECT name,SDO_GEOM.SDO_AREA(shape,0.005) 
+	FROM cola_mrp;
 
 -- d) Find area of only one location.
-SELECT c.name,SDO_GEOM.SDO_AREA(c.shape,0.005) FROM cola_mrp c
-WHERE c.name='cola_a';
+SELECT c.name,SDO_GEOM.SDO_AREA(c.shape,0.005)
+	FROM cola_mrp c
+	WHERE c.name='cola_a';
 
 -- e) Find the distance between two geometries.
 SELECT SDO_GEOM.SDO_DISTANCE(c_b.shape,c_d.shape,0.005)
-FROM cola_mrp c_b,cola_mrp c_d
-WHERE c_b.name='cola_b'AND c_d.name='cola_d';
+	FROM cola_mrp c_b,cola_mrp c_d
+	WHERE c_b.name='cola_b'AND c_d.name='cola_d';
