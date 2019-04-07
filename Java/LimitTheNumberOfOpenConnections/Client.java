c13413791b766b7babf4b075b8691bbb06c3a2ca
Bhupend Patil <bhupendpatil@gmail.com>
2019-04-07 19:55:09 +0530

    Limit The Number Of Open Connections

---
+++ Java/LimitTheNumberOfOpenConnections/Client.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+
+class client4 {
+
+    public static void main(String args[]) {
+        try {
+            Socket s = new Socket("localhost", 4040);
+
+            InputStream in = s.getInputStream();
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+ 
+            OutputStream out = s.getOutputStream();
+            PrintWriter pw = new PrintWriter(out, true);
+
+            InputStreamReader isr1 = new InputStreamReader(System.in);
+            BufferedReader br1 = new BufferedReader(isr1);
+
+              while (true) {
+                    System.out.println("CAndroid/CIOS/CQuit");
+                    String line = br1.readLine();
+                    
+                    if(line.equalsIgnoreCase("CAndroid")){
+                        System.out.println("Sejal");
+                    } else if(line.equalsIgnoreCase("CIOS")){
+                        System.out.println("Anam");
+                    } else if(line.equalsIgnoreCase("CQuit")){
+                        System.out.println("Let's Go Home");
+                        System.exit(0);
+                    }
+               }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Java/LimitTheNumberOfOpenConnections/Server.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.io.*;
+
+class server4 {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(4040, 50);
+            Socket[] client = new Socket[10];
+
+            for (int i = 0; i < client.length; i++) {
+                client[i] = new Socket("localhost", 4040);
+                client[i] = ss.accept();
+                System.out.printf("Client %2d: " + client[i] + "%n", i);
+
+                InputStream in = client[i].getInputStream();
+                InputStreamReader isr = new InputStreamReader(in);
+                BufferedReader br = new BufferedReader(isr);
+
+                OutputStream out = client[i].getOutputStream();
+                PrintWriter pw = new PrintWriter(out, true);
+
+                InputStreamReader isr1 = new InputStreamReader(System.in);
+                BufferedReader br1 = new BufferedReader(isr1);
+
+                while (true) {
+                    System.out.println("SAndroid/SIOS/SQuit");
+                    String line = br1.readLine();
+                    
+                    if(line.equalsIgnoreCase("SAndroid")){
+                        System.out.println("Sejal");
+                    } else if(line.equalsIgnoreCase("SIOS")){
+                        System.out.println("Anam");
+                    } else if(line.equalsIgnoreCase("SQuit")){
+                        System.out.println("Let's Go Home");
+                        client[i].close();
+                        System.exit(0);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
