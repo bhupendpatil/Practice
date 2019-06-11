5715d6a5f394c03072ff6519e103f35d84e0b7e8
Bhupend Patil <bhupendpatil@gmail.com>
2019-06-11 19:26:16 +0530

    Generate SPM

---
+++ C++/SPM.cpp
@@ -0,0 +1,513 @@
+#include<stdio.h>
+#include<conio.h>
+#include<alloc.h>
+#include<string.h>
+
+void main() {
+	char **at,sort[100],ch,ntt[100],**less1,**great2,**equal1,**parse;
+	int i,j,n,q,s,a,u,m,l,p=0,c,k=0,y,z=0,d,mx,my;
+	int **first,**equal,**last,**trans,**lastp,**firstp,**firsts,**less,**great,**great1;
+	clrscr();
+	printf("\n How Many Production You Want:");
+	scanf("%d",&n);
+	at=(char **)malloc(n*sizeof(int *));
+
+	for(i=0;i<n;i++){
+		at[i]=(char *)malloc(2*n);
+	}
+	printf("Enter a production:");
+	for(i=0;i<n;i++){
+		scanf("%s",&at[i][0]);
+	}
+	for(i=0;i<n;i++){
+		c=strlen(at[i]);
+		for(j=0;j<c;j++){
+			sort[p]=at[i][j];
+			p++;
+			q=p;
+		}
+	}
+
+	for(p=0;p<q;p++){
+		for(s=p+1;s<q;s++){
+			if(sort[p]>=sort[s]){
+				u=sort[p];
+				sort[p]=sort[s];
+				sort[s]=u;
+			}
+		}
+	}
+
+	for(p=0;p<q;p++){
+		ch=sort[p];
+		if(ch!=sort[p+1]){
+			if(ch!='='){
+				sort[k]=ch;
+				ntt[z]=sort[k];
+				//printf("%c",ntt[z]);
+				k++;
+				z++;
+			}
+		}
+		d=z;
+	}
+
+	printf("\nTotal no of non terminal& terminal:");
+	for(i=0;i<d;i++){
+		printf("%c",ntt[i]);
+	}
+	getch();
+	first=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		first[i]=(int *)malloc(2*d);
+	}
+	firstp=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		firstp[i]=(int *)malloc(2*d);
+	}
+	firsts=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		firsts[i]=(int *)malloc(2*d);
+	}
+	less=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		less[i]=(int *)malloc(2*d);
+	}
+	great=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		great[i]=(int *)malloc(2*d);
+	}
+	great1=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		great1[i]=(int *)malloc(2*d);
+	}
+	printf("\n");
+
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			first[i][j]=0;
+			trans[i][j]=0;
+			firstp[i][j]=0;
+			firsts[i][j]=0;
+			less[i][j]=0;
+			great[i][j]=0;
+			great1[i][j]=0;
+		}
+	}
+
+	for(i=0;i<n;i++){
+		ch=at[i][0];
+		for(m=0;m<d;m++){
+			if(ch==ntt[m]){
+				mx=m;
+			}
+		}
+		ch=at[i][2];
+		for(a=0;a<d;a++){
+			if(ch==ntt[a]){
+				my=a;
+			}
+		}
+		first[mx][my]=1;
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tFIRST MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",first[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+	//for first plus
+	i=0;
+	while(i<d){
+		for(j=0;j<d;j++){
+			if(first[j][i]==1){
+				for(k=0;k<d;k++){
+					first[j][k]=(first[j][k] || first[i][k]);
+					firstp[j][k]=first[j][k];
+					firsts[j][k]=firstp[j][k];
+				}
+			}
+		}
+		i++;
+	}
+	clrscr();
+
+	printf("\n\n\n\t\t\tFIRST PLUS MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",firstp[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	// for first *
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(i==j){
+				first[i][j]=1;
+				firsts[i][j]=first[i][j];
+			}
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tFIRST STAR MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",firsts[i][j]);
+		}
+		printf("\n");
+	}
+
+	getch();
+
+	//for equal matrix
+	equal=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		equal[i]=(int *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			equal[i][j]=0;
+		}
+	}
+	for(i=0;i<n;i++){
+		c=strlen(at[i]);
+		if(c>3){
+			for(j=2;j<c-1;j++){
+				ch=at[i][j];
+				for(m=0;m<d;m++){
+					if(ch==ntt[m]){
+						mx=m;
+					}
+				}
+				ch=at[i][j+1];
+				for(a=0;a<d;a++){
+					if(ch==ntt[a]){
+						my=a;
+					}
+				}
+				equal[mx][my]=1;
+			}
+		}
+	}
+	equal1=(char **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		equal1[i]=(char *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			equal1[i][j]='0';
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(equal[i][j]>0){
+				equal1[i][j]='=';
+			}
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tEQUAL MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2c",equal1[i][j]);
+		}
+		printf("\n");
+	}
+
+	getch();
+
+	// For Last
+	last=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		last[i]=(int *)malloc(2*d);
+	}
+	printf("\n");
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			last[i][j]=0;
+		}
+	}
+	for(i=0;i<n;i++){
+		c=strlen(at[i]);
+		ch=at[i][0];
+		for(m=0;m<d;m++){
+			if(ch==ntt[m]){
+				mx=m;
+			}
+		}
+		ch=at[i][c-1];
+		for(a=0;a<d;a++){
+			if(ch==ntt[a]){
+				my=a;
+			}
+		}
+		last[mx][my]=1;
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tLAST MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",last[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	//for Last plus
+	lastp=(int **)malloc(d*sizeof(int *));
+
+	for(i=0;i<d;i++){
+		lastp[i]=(int *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			lastp[i][j]=0;
+		}
+	}
+	
+	i=0;
+	while(i<d){
+		for(j=0;j<d;j++){
+			if(last[j][i]==1){
+				for(k=0;k<d;k++){
+					last[j][k]=(last[j][k] || last[i][k]);
+					lastp[j][k]=last[j][k];
+				}
+			}
+		}
+		i++;
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tLAST PLUS MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",lastp[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	// for last *
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(i==j){
+				last[i][j]=1;
+			}
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tLAST STAR MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",last[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+   
+   // for Last Transposition
+	trans=(int **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		trans[i]=(int *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			trans[i][j]=0;
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			trans[j][i]=lastp[i][j];
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tTRANSPOSION OF LAST PLUS MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2d",trans[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	// FOR LESS THAN
+	less1=(char **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		less1[i]=(char *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			less1[i][j]='0';
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			less[i][j]=0;
+			for(k=0;k<d;k++){
+				less[i][j]+=equal[i][k]*firstp[k][j];
+			}
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(less[i][j]>0){
+				less1[i][j]='<';
+			}
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tLESS THAN MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2c",less1[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	//FOR GREATER
+	great2=(char **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		great2[i]=(char *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			great2[i][j]='0';
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			great[i][j]=0;
+			for(k=0;k<d;k++){
+				great[i][j]+=trans[i][k]*equal[k][j];
+			}
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+		//great1[i][j]=0;
+			for(k=0;k<d;k++){
+				great1[i][j]+=great[i][k]*firsts[k][j];
+			}
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(great1[i][j]>0){
+				great2[i][j]='>';
+			}
+		}
+	}
+
+	clrscr();
+	printf("\n\n\n\t\t\tGREATER THAN MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2c",great2[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+
+	//For Super Impose
+	clrscr();
+	parse=(char **)malloc(d*sizeof(int *));
+	for(i=0;i<d;i++){
+		parse[i]=(char *)malloc(2*d);
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			parse[i][j]='0';
+		}
+	}
+	for(i=0;i<d;i++){
+		for(j=0;j<d;j++){
+			if(less1[i][j]=='<'){
+				parse[i][j]='<';
+			}
+			if(great2[i][j]=='>'){
+				parse[i][j]='>';
+			}
+			if(equal1[i][j]=='='){
+				parse[i][j]='=';
+			}
+		}
+	}
+	clrscr();
+	printf("\n\n\n\t\t\tSIMPLE PRECEDENCE MATRIX:\n\n");
+	printf("\n\t\t\t");
+	for(j=0;j<d;j++)
+		printf("%2c",ntt[j]);
+	printf("\n");
+	for(i=0;i<d;i++){
+		printf("\n\t\t\t%c",ntt[i]);
+		for(j=0;j<d;j++){
+			printf("%2c",parse[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+}
---
+++ Java/SPM.java
@@ -0,0 +1,326 @@
+class SPM1{
+  SPM1(){}
+
+  void PrintProduction(String Prod[]){
+    System.out.println();
+    for(int i=0;i<4;i++){
+      System.out.println("\t"+Prod[i]);
+    }
+    System.out.println("\n");
+  }
+
+  void printSubProd(String Prod[]){
+    System.out.println();
+    for(int i=0;i<5;i++){
+      System.out.println("\t"+Prod[i]);			
+    }
+    System.out.println("\n");
+  }
+
+  void displayMatrix(int Matrix[][],String strString){
+    System.out.print("\t");
+    for(int i=0;i<7;i++)
+      System.out.print("   "+strString.charAt(i));
+
+    for(int i=0;i<7;i++){
+      System.out.println();
+      System.out.print("        "+strString.charAt(i)+"  ");
+      for(int j=0;j<7;j++){
+        System.out.print(Matrix[i][j]+"   ");
+      }
+    }
+  }
+}
+
+class SPM{
+  public static void main(String args[]){
+    int i,j,k,l=0;
+
+    SPM1 s = new SPM1(); 
+    String Prod[]=new String[4];
+    Prod[0]="Z->bMb";
+    Prod[1]="M->(L";
+    Prod[2]="M->a";
+    Prod[3]="L->Ma)";
+
+    String NT[]={"Z","M","L"};
+    String T[]={"a","b","(",")"}; 
+    String first[]=new String[4];
+    String last[]=new String[4];
+    String equal[]=new String[5];
+
+    int first_matrix[][]=new int[7][7];
+    int last_matrix[][]=new int[7][7];
+    int equal_matrix[][]=new int[7][7];
+    String SPM[][]=new String[7][7] ;
+
+    int A[][]=new int[7][7];
+		int B[][]=new int[7][7];
+		int C[][]=new int[7][7];
+		int D[][]=new int[7][7];
+		int E[][]=new int[7][7];
+		int I[][]=new int[7][7];
+		int TlastPlus_matrix[][]=new int[7][7];
+    int lessthan_matrix[][]=new int[7][7];  
+    int greterthan_matrix[][]=new int[7][7];          			
+    String strString="ZbMLa()";
+
+    //Displaying of productins.
+    System.out.println("\n"+"\t"+"Practical No:04"+"\t");
+    System.out.println("\n");
+    System.out.println("\t"+"Simple Precedence Matrix"+"\t");
+    System.out.println("\n Productions are::");
+    s.PrintProduction(Prod);
+    
+    //Finding Elements of First
+    for(i=0;i<4;i++){
+      first[i]=Prod[i].charAt(0)+""+Prod[i].charAt(3);
+    }
+
+    //Displaying of First elements
+    System.out.println("First Elements ::");
+    s.PrintProduction(first);
+    System.out.println("First Matrix ::");
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        first_matrix[i][j]=0;
+      }
+    }
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        for(k=0;k<4;k++){
+          if(strString.charAt(i)==first[k].charAt(0)&&strString.charAt(j)==first[k].charAt(1)){
+            first_matrix[i][j]=1;
+          }
+        }
+      }
+    }
+    s.displayMatrix(first_matrix,strString);
+   
+    //Appliying warshall algorithm.
+    //Displaying Transitive Closure of first 
+    System.out.println("\n\n"+"first+ matrix is:");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        A[i][j]=first_matrix[i][j];
+      }
+    }
+    for(i=1;i<7;i++){
+      for(j=0;j<7;j++){
+        if(A[j][i]==1){
+          for(k=1;k<7;k++){
+            A[j][k]=A[j][k]|A[i][k];
+          }
+        }
+      }
+    }
+    s.displayMatrix(A,strString);
+
+    //Displaying first * matrix
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        if(i==j)
+          I[i][j]=1;
+        else
+          I[i][j]=0;
+      }
+    }
+
+    System.out.println("\n\n"+"first*_matrix is;");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        C[i][j]=A[i][j]|I[i][j];
+      }
+    }
+    s.displayMatrix(C,strString);
+
+    //Displaying first * matrix
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        if(i==j)
+          I[i][j]=1;
+        else
+          I[i][j]=0;
+      }
+    }
+    System.out.println("\n\n"+"first*_matrix is;");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        C[i][j]=A[i][j]|I[i][j];
+      }
+    }
+    s.displayMatrix(C,strString);	
+
+    //Finding the elements of Last
+    for(i=0;i<Prod.length;i++){
+      last[i]=Prod[i].charAt(0)+""+Prod[i].charAt(Prod[i].length()-1);			
+    }
+
+    //Displaying of Last elements
+    System.out.println();
+    System.out.println("\nLast Elements ::");
+    s.PrintProduction(last);
+    System.out.println("Last Matrix ::");
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        last_matrix[i][j]=0;
+      }
+    }
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        for(k=0;k<4;k++){
+          if(strString.charAt(i)==last[k].charAt(0)&& strString.charAt(j)==last[k].charAt(1)){
+            last_matrix[i][j]=1;
+          }
+        }
+      }
+    }
+    s.displayMatrix(last_matrix,strString);	
+
+    //Appliying warshall algorithm.
+    //Displaying of Transitive Closure last
+    System.out.println("\n"+"last+ matrix is:");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        B[i][j]=last_matrix[i][j];
+      }
+    }
+    for(i=1;i<7;i++){
+      for(j=0;j<7;j++){
+        if(B[j][i]==1){
+          for(k=1;k<7;k++){
+            B[j][k]=B[j][k]|B[i][k];
+          }
+        }
+      }
+    }
+    s.displayMatrix(B,strString);
+
+    System.out.println("\n"+"last*_matrix is;");
+    System.out.println();
+
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        E[i][j]=B[i][j]|I[i][j];
+      }
+    }
+    s.displayMatrix(E,strString);
+
+    //Finding the elements of equal
+    int pos=Prod[0].indexOf(">");
+    for(i=0;i<Prod.length;i++){
+      String str=Prod[i].substring(pos+1);
+      if(str.length()>=2){
+        for(j=0;j<str.length()-1;j++){
+          equal[l]=str.charAt(j)+""+str.charAt(j+1);
+          System.out.print(""+equal[l]);
+          l++;
+        }
+      }
+    }
+
+    //Displaying of equal elements
+    System.out.println();
+    System.out.println("\nequal Elements ::");
+    s.printSubProd(equal);
+    System.out.println("equal Matrix ::");                
+    for(i=0;i<equal_matrix.length;i++){
+      for(j=0;j<equal_matrix.length;j++){
+        equal_matrix[i][j]=0;
+      }
+    }
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        for(k=0;k<5;k++){
+          if(strString.charAt(i)==equal[k].charAt(0)&&strString.charAt(j)==equal[k].charAt(1)){
+            equal_matrix[i][j]=1;
+          }
+        }
+      }
+    }
+    s.displayMatrix(equal_matrix,strString);
+
+    //Displaying less than matrix
+    System.out.println("\n"+"less_than matrix is;");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        lessthan_matrix[i][j] = equal_matrix[i][0]*A[0][j]+
+                                equal_matrix[i][1]*A[1][j]+
+                                equal_matrix[i][2]*A[2][j]+
+                                equal_matrix[i][3]*A[3][j]+
+                                equal_matrix[i][4]*A[4][j]+
+                                equal_matrix[i][5]*A[5][j]+
+                                equal_matrix[i][6]*A[6][j];
+      }
+    }
+    s.displayMatrix( lessthan_matrix,strString);
+
+    //Displaying transpose of lastplus
+    //System.out.println("\n"+"transpose of last+ is;");
+    //System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        TlastPlus_matrix[i][j]=B[j][i];
+      }
+    }
+
+    //s.displayMatrix( TlastPlus_matrix,strString);
+
+    //Displaying less than matrix
+    System.out.println("\n"+"greter_than matrix is;");
+    System.out.println();
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        D[i][j]=TlastPlus_matrix[i][0]*equal_matrix[0][j]+
+                TlastPlus_matrix[i][1]*equal_matrix[1][j]+
+                TlastPlus_matrix[i][2]*equal_matrix[2][j]+
+                TlastPlus_matrix[i][3]*equal_matrix[3][j]+
+                TlastPlus_matrix[i][4]*equal_matrix[4][j]+
+                TlastPlus_matrix[i][5]*equal_matrix[5][j]+
+                TlastPlus_matrix[i][6]*equal_matrix[6][j];
+      }
+    }
+
+    for(i=0;i<7;i++){
+      for(j=0;j<7;j++){
+        greterthan_matrix[i][j] = D[i][0]*C[0][j]+
+                                  D[i][1]*C[1][j]+
+                                  D[i][2]*C[2][j]+
+                                  D[i][3]*C[3][j]+
+                                  D[i][4]*C[4][j]+
+                                  D[i][5]*C[5][j]+
+                                  D[i][6]*C[6][j];
+      }
+    }
+    s.displayMatrix(greterthan_matrix,strString);
+
+    //Displaying SPM matrix
+    System.out.println("\n"+"SPM matrix is;");
+    System.out.println();
+    System.out.print("\t");
+    for(i=0;i<7;i++)
+      System.out.print("   "+strString.charAt(i));
+
+    for(i=0;i<7;i++){
+      System.out.println();
+      System.out.print("        "+strString.charAt(i)+"  ");
+      for(j=0;j<7;j++){
+        if(equal_matrix[i][j]==1)
+          SPM[i][j]="=";
+        else if(lessthan_matrix[i][j]==1)
+          SPM[i][j]="<";
+        else if(greterthan_matrix[i][j]==1)
+          SPM[i][j]=">";
+        else 
+          SPM[i][j]="0";
+        System.out.print(SPM[i][j]+"   ");
+      }
+    }
+    //s.displayMatrix(SPM,strString);
+  }
+}
