d462ff4aee08dba4986c083f57a77a2b0aeaf5cd
Bhupend Patil <bhupendpatil@gmail.com>
2019-06-25 17:30:36 +0530

    cleanup

--- Java/SocketMessage/Client.java
+++ Java/SocketMessage/Client.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client{
     private static Socket socket;
--- Java/SocketMessage/Server.java
+++ Java/SocketMessage/Server.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Server {
     private static Socket socket;
