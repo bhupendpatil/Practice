4ddf535d2ba29bcc46763661393297511dce4778
Bhupend Patil <bhupendpatil@gmail.com>
2020-01-12 23:45:23 +0530

    updated code

--- Java/CloseableSimpleWarning.java
+++ Java/CloseableSimpleWarning.java
@@ -1,42 +1,39 @@
+package closeablesimplewarning;
+
 import java.awt.*;
 import java.awt.event.*;
 
 class CloseableSimpleWarning extends Frame implements WindowListener {
-  Label l1;
 
   public CloseableSimpleWarning(){
     setTitle("Warning");
     setSize(200,300);
     addWindowListener(this);
-
-    l1 = new Label("Hello");
-    this.add(l1);
   }
 
   public void windowClosing(WindowEvent e){
-    l1.setText("Closing");
+    System.out.println("Closing");
     System.exit(0);
   }
 
   public void windowClosed(WindowEvent e){
-    System.out.println("Close");
+    System.out.println("Close/Minimized");
   }
 
   public void windowIconified(WindowEvent e){
-    l1.setText("Iconified");
+    System.out.println("Iconified");
   }
 
   public void windowDeiconified(WindowEvent e) {
-    l1.setText("dE-Iconified");
+    System.out.println("dE-Iconified");
   }
 
   public void windowOpened(WindowEvent e){
-    l1.setText("Open");
     System.out.println("Open");
   }
 
   public void windowActivated(WindowEvent e){
-    l1.setText("Open");
+    System.out.println("Activated");
   }
 
   public void windowDeactivated(WindowEvent e){
