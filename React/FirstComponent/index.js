164715105642501a25eae15040421d0f360b3d4f
Bhupend Patil <bhupendpatil@gmail.com>
2020-06-08 14:33:29 +0530

    First Component

---
+++ React/FirstComponent/index.js
@@ -0,0 +1,13 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Bhupend!</h1>
+                <h1>Hello Bhupend!</h1>
+                <h1>Hello Bhupend!</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
