a5641d0d0a7f27a501669eac2184bfc37b16b7de
Bhupend Patil <bhupendpatil@gmail.com>
2020-06-09 20:15:53 +0530

    default Props

--- React/JSXProps/Hello.js
+++ React/JSXProps/Hello.js
@@ -1,4 +1,8 @@
 class Hello extends React.Component {
+    static defaultProps = {
+        from : "Anonymous",
+        bangs : 1
+    }
     render() {
         let bangs = "!".repeat(this.props.bangs);
         return (
--- React/JSXProps/index.js
+++ React/JSXProps/index.js
@@ -4,6 +4,9 @@ class App extends React.Component {
             <div>
                 <Hello
                     to="Bhupend"
+                />
+                <Hello
+                    to="Bhupend"
                     from="Someone"
                     bangs={4}
                     img="https://images.unsplash.com/photo-1588774210246-a1dc467758df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
