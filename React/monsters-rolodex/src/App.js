e748780be43e34f1b26aed398e5da7592cb739f3
Bhupend Patil <bhupendpatil@gmail.com>
2020-06-12 14:53:15 +0530

    first React Project

---
+++ React/monsters-rolodex/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
