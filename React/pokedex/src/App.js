079637e3160e122f3a37896b7c373d0296e1442e
Bhupend Patil <bhupendpatil@gmail.com>
2020-06-19 00:51:34 +0530

    added pokegame

---
+++ React/pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Pokegame from "./components/pokegame/pokegame.component";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
