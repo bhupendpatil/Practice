14fab16b837321d5450e43e5e636b0e2297c3c64
Bhupend Patil <bhupendpatil@gmail.com>
2020-08-12 23:22:37 +0530

    Getting Input from Users

---
+++ Node.js/getting-input-from-users/app.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+const getNotes = require('./notes.js')
+
+const command = process.argv[2]
+
+if (command == 'add'){
+    console.log('Adding note!')
+} else if (command == 'remove') {
+    console.log('Removing note!')
+}
