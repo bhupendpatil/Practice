eb288fdfe035b8df4c247baaa540244e936460be
Bhupend Patil <bhupendpatil@gmail.com>
2020-08-13 21:20:36 +0530

    added yargs.parse and builder

--- Node.js/argument-parsing-with-yargs/app.js
+++ Node.js/argument-parsing-with-yargs/app.js
@@ -10,8 +10,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body : {
+            description: 'Note body',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -42,4 +55,4 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
