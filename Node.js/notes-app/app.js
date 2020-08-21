546b1384926d3df4f919bebc2ea65ebcc29729e1
Bhupend Patil <bhupendpatil@gmail.com>
2020-08-22 00:05:13 +0530

    refactored

--- Node.js/notes-app/app.js
+++ Node.js/notes-app/app.js
@@ -20,7 +20,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -37,7 +37,7 @@ yargs.command({
 
         }
     },
-    handler: function (argv) {
+    handler(argv) { 
         notes.removeNote(argv.title)
     }
 })
@@ -46,16 +46,16 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
+    handler() {
         console.log('Listing out all notes')
-    }
+    }   
 })
 
 // Create read command
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
+    handler(){
         console.log('Reading a note')
     }
 })
