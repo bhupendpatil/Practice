6a8b80bad0a64ecaba909ba465602929ed7c8769
Bhupend Patil <bhupendpatil@gmail.com>
2020-08-25 16:57:42 +0530

    nul

--- Node.js/arrow-function.js
+++ Node.js/arrow-function.js
@@ -15,7 +15,6 @@ const event = {
     guestList: ['Andrew','Jen','Mike'],
     printGuestList() {
         console.log('Guest list for '+ this.name)
-
         this.guestList.forEach((guest) => {
             console.log(guest + ' is attending ' + this.name)
         })
