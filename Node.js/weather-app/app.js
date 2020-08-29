e10138f3177e4e90b9cc435e0f7114a567bc2835
Bhupend Patil <bhupendpatil@gmail.com>
2020-08-29 11:09:36 +0530

    del

--- Node.js/weather-app/app.js
+++
@@ -1,8 +0,0 @@
-const request = require('request')
-
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=01e53f477bdf75e22b9f6af871249a9e'
-
-request({url:url},(error,response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.weather[0].main)
-})
