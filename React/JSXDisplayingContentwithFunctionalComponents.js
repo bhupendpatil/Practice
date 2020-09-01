81421242c3471eac024be0e7be37f77d336ed07e
Bhupend Patil <bhupendpatil@gmail.com>
2020-09-01 14:42:47 +0530

    jsx basic

---
+++ React/JSXDisplayingContentwithFunctionalComponents.js
@@ -0,0 +1,11 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
