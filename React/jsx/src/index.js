506577b850a96816a489f295e9d18c4a8f55dac3
Bhupend Patil <bhupendpatil@gmail.com>
2020-09-03 00:23:45 +0530

    hidden

--- React/jsx/src/index.js
+++ React/jsx/src/index.js
@@ -4,19 +4,17 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 
-function getButtonText() {
-  return "Click on me!";
-}
-
 const App = () => {
+  const buttonText = "Click me!";
+
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter name:
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
+        {buttonText}
       </button>
     </div>
   );
