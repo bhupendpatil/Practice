6549bb21840f46903d4fda497cee74bb3f736eab
Bhupend Patil <bhupendpatil@gmail.com>
2020-09-03 17:52:30 +0530

    init

---
+++ React/components/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
