5c0461febc9a227e2e6e71c5410fa88e078e3c21
Bhupend Patil <bhupendpatil@gmail.com>
2020-09-03 19:26:40 +0530

    passing and receiving props

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -5,10 +5,10 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Bhupend" />
+      <CommentDetail author="Harshad" />
+      <CommentDetail author="Hitesh" />
+      <CommentDetail author="None" />
     </div>
   );
 };
