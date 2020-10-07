92ab02352038bd4b01b08172dfb785168b0e47be
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-07 23:10:47 +0530

    added multiple props

--- React/components/src/CommentDetail.js
+++ React/components/src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
