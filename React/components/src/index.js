3cd716c3cb9e89865e51a382ef6aef76521c4885
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-08 18:09:40 +0530

    Approval Card

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Bhupend"
         timeAgo="Today at 4:45PM"
