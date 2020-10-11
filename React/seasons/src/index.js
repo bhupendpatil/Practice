b94aec0d6142f4696e1530518fd52011bf965648
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-11 21:26:21 +0530

    class

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
