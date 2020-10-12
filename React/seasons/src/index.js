df03e9c05fbfea6b893b34acc2e5456167df862d
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-12 22:53:48 +0530

    state init

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
