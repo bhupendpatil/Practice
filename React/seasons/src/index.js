f81cbed66352bedf1372b49e06088decd7adef4e
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-13 00:59:08 +0530

    latitude

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -6,15 +6,17 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
