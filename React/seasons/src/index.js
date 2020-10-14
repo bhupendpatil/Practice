a932a0cb8d954ef9041acfea034e0adb07561dfc
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-15 00:22:22 +0530

    componentDidMount method

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -6,14 +6,12 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
