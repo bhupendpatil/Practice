a682288752f69c9a76bb994e28cabbc4938a90b6
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-16 03:58:03 +0530

    added icons

--- React/seasons/src/SeasonDisplay.js
+++ React/seasons/src/SeasonDisplay.js
@@ -12,7 +12,15 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text =
     season === "summer" ? "Let's hit the beach" : "Burr, it's chilly!!!";
-  return <div>{text}</div>;
+  const icon = season === "summer" ? "sun" : "snowflake";
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
