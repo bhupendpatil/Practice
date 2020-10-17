8be2ca720ddae575527d3c953ee20e81163d44ff
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-17 19:40:30 +0530

    added spinner

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -19,7 +20,7 @@ class App extends React.Component {
     if (!this.state.lat && this.state.errorMessage) {
       return <div>Error : {this.state.errorMessage}</div>;
     }
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
