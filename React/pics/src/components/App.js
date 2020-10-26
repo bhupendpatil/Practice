9be7c08c80f19c0590eb872492130b087bf76316
Bhupend Patil <bhupendpatil@gmail.com>
2020-10-27 00:13:45 +0530

    fix

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={thisonSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
