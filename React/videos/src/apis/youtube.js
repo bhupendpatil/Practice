0f2779df7ab1b8133de8088ff2e054027cef08cc
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-03 14:27:18 +0530

    config

--- React/videos/src/apis/youtube.js
+++ React/videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBtuRkFmw6F2PBy6PGEVwyoEBWqKhVaqQE";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
