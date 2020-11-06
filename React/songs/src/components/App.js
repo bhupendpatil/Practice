38209e1ffebb6f2ba94d598e43f16abbe8080575
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-06 22:44:08 +0530

    connect

--- React/songs/src/components/App.js
+++ React/songs/src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { selectSong } from "../actions";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
