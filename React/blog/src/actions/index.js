0755cca0ba842dd722db26c7e803064d19ea8cf9
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-09 14:46:17 +0530

    not neat way

--- React/blog/src/actions/index.js
+++ React/blog/src/actions/index.js
@@ -1,10 +1,10 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => {
+  const promise = jsonPlaceholder.get("/posts");
 
   return {
     type: "FETCH_POSTS",
-    payload: response,
+    payload: promise,
   };
 };
