41d9ea242734900818e006953abf801cde54459e
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-11 17:40:50 +0530

    feature normalize

--- ML/ex1 Linear Regression/featureNormalize.m
+++ ML/ex1 Linear Regression/featureNormalize.m
@@ -7,8 +7,8 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+%mu = zeros(1, size(X, 2));
+%sigma = zeros(1, size(X, 2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -39,7 +39,6 @@ sigma = zeros(1, size(X, 2));
 
 mu = mean(X);
 X_norm = X_norm - mu;
-
 sigma = std(X);
 X_norm = X_norm ./ sigma;
 
