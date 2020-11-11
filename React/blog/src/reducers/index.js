6551f32ce91e0eb07e39bbf7725d9dbeb63637d6
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-11 23:42:04 +0530

    post reducer

--- React/blog/src/reducers/index.js
+++ React/blog/src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import postReducer from "./postReducer";
 
-export default combineReducers({ tp: () => "del me" });
+export default combineReducers({
+  posts: postReducer,
+});
