5ff09ad12abba55cd16022f8aa31b7f5f240979a
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-11 13:58:15 +0530

    Surface Plot, Contour Plot

--- ML/ex1 Linear Regression/LineByLineAll.m
+++ ML/ex1 Linear Regression/LineByLineAll.m
@@ -49,3 +49,32 @@ predict2 = [1,7] * theta;
 fprintf('For population = 35k, we predict a profit of %f\n', predict1*10000);
 fprintf('For population =70k, we predict a profit of %f\n', predict2*10000);
 
+
+% Visualizing J(theta_0, theta_1):
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
+disp('theta0_vals and theta1_vals set');
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
+for i = 1:length(theta0_vals)
+    for j = 1:length(theta1_vals)
+	  t = [theta0_vals(i); theta1_vals(j)];    
+	  J_vals(i,j) = computeCost(X, y, t);
+    end
+end
+J_vals = J_vals';
+disp('J_vals set to transpose');
+
+% Surface Plot
+disp('Surface Plot displayed');
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1');
+
+% Contour Plot
+disp('Contour Plot displayed');
+figure;
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('\theta_0'); ylabel('\theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+hold off;
