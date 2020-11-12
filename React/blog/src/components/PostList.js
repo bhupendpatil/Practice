3eb1a18d7b36dd09ddb84e9e2266a2c72ef6b202
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-12 13:36:16 +0530

    fix

--- React/blog/src/components/PostList.js
+++ React/blog/src/components/PostList.js
@@ -13,7 +13,7 @@ class PostList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { posts: state.props };
+  return { posts: state.posts };
 };
 
 export default connect(mapStateToProps, { fetchPosts })(PostList);
