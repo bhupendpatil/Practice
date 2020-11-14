e827583003e0a52bd910a99ed75abfd0398ee9d2
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-14 20:08:27 +0530

    original state

--- React/blog/src/actions/index.js
+++ React/blog/src/actions/index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+//import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -7,9 +7,15 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
