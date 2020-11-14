52844a7b32e2f887a633e1b6be4a24c655e6078a
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-14 19:50:59 +0530

    one time memoization

--- React/blog/src/actions/index.js
+++ React/blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -6,8 +7,9 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
