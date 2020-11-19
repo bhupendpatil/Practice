60d83cf66e07ae664e30b8a232266282b52dbede
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-19 16:23:29 +0530

    setting always visible header

--- React/streams/client/src/components/App.js
+++ React/streams/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -8,9 +9,10 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
