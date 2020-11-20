63edf50518753c04b32d7b91501137c4e17cf9c8
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-21 02:45:28 +0530

    one vs all prediction

--- ML/ex3 Multi-class Classification and Neural Networks/predictOneVsAll.m
+++ ML/ex3 Multi-class Classification and Neural Networks/predictOneVsAll.m
@@ -8,14 +8,14 @@ function p = predictOneVsAll(all_theta, X)
 %  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
 %  for 4 examples) 
 
-m = size(X, 1);
-num_labels = size(all_theta, 1);
+%m = size(X, 1);
+%num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
-X = [ones(m, 1) X];
+X = [ones(size(X, 1), 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -31,9 +31,7 @@ X = [ones(m, 1) X];
 %       
 
 
-
-
-
+[val p] = max(sigmoid(X*all_theta'),[],2);
 
 
 % =========================================================================
