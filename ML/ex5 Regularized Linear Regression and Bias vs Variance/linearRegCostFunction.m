03bd5b17a313abec5cd78722fe92a153e653be51
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-26 11:52:32 +0530

    linear regression

--- ML/ex5 Regularized Linear Regression and Bias vs Variance/linearRegCostFunction.m
+++ ML/ex5 Regularized Linear Regression and Bias vs Variance/linearRegCostFunction.m
@@ -21,12 +21,11 @@ grad = zeros(size(theta));
 
 
 
+J = ((1/(2*m)) * (sum(((X * theta)-y).^2))) + ((lambda/(2*m)) * (sum(theta(2:end).^2)));
 
+grad(1) = (1/m) * (X(:,1)' * ((X * theta)-y));
 
-
-
-
-
+grad(2:end) = (1/m) * (X(:,2:end)' * ((X * theta)-y)) + ((lambda/m) * theta(2:end));
 
 
 
