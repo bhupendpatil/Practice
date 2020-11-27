0bb7a924ea18e90b11555f9e8b3e5c0f077cd5b9
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-27 14:10:50 +0530

    automatically handling events

--- React/streams/client/src/components/streams/StreamCreate.js
+++ React/streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
   render() {
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
