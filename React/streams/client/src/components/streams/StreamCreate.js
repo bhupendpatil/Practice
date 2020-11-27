63cb88cb768a11e7b41c77416b242abd0fbef10d
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-27 14:55:25 +0530

    highlighting errored fields

--- React/streams/client/src/components/streams/StreamCreate.js
+++ React/streams/client/src/components/streams/StreamCreate.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        <div>{this.renderError(meta)}</div>
       </div>
     );
-  }
+  };
 
   onSubmit(formValue) {
     console.log(formValue);
@@ -19,7 +30,7 @@ class StreamCreate extends React.Component {
   render() {
     return (
       <form
-        className="ui form"
+        className="ui form error"
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
