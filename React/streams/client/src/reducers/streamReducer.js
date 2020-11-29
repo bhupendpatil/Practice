cb0369cc89443ed50cbe6145bd895ef8e0ab5fd8
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-29 13:31:46 +0530

    fixed bug

--- React/streams/client/src/reducers/streamReducer.js
+++ React/streams/client/src/reducers/streamReducer.js
@@ -6,7 +6,7 @@ import {
   CREATE_STREAM,
   EDIT_STREAM,
   DELETE_STREAM,
-} from ".../actions/types";
+} from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
