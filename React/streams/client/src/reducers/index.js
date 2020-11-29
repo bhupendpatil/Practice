34ce08ea32fb530ba774df1b6fb61359247a7854
Bhupend Patil <bhupendpatil@gmail.com>
2020-11-29 13:21:22 +0530

    merging lists of record

--- React/streams/client/src/reducers/index.js
+++ React/streams/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as fromReducer } from "redux-form";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: fromReducer,
+  streams: streamReducer,
 });
--- React/streams/client/src/reducers/streamReducer.js
+++ React/streams/client/src/reducers/streamReducer.js
@@ -10,6 +10,8 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
