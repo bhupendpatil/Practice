2e512408a98fcfb041013e4766410f9139ddf31d
Bhupend Patil <bhupendpatil@gmail.com>
2020-12-03 00:16:44 +0530

    selecting records from state

--- React/streams/client/src/components/streams/StreamEdit.js
+++ React/streams/client/src/components/streams/StreamEdit.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const StreamEdit = (props) => {
   console.log(props);
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps)(StreamEdit);
