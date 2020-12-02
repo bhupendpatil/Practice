4f9cb3d79d94df15bab84e403906d97367401f99
Bhupend Patil <bhupendpatil@gmail.com>
2020-12-02 15:48:03 +0530

    implementing navigation

--- React/streams/client/src/actions/index.js
+++ React/streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
