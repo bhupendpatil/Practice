5e4339c7dc6693b83a786d312308c067d17d9639
Bhupend Patil <bhupendpatil@gmail.com>
2020-12-03 16:55:27 +0530

    edit form submission

--- React/streams/client/src/actions/index.js
+++ React/streams/client/src/actions/index.js
@@ -47,6 +47,7 @@ export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
