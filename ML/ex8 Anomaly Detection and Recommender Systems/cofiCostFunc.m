1938b978adfa1884c476d8246a46f341987fa0c2
Bhupend Patil <bhupendpatil@gmail.com>
2020-12-05 01:05:38 +0530

    regularized gradient

--- ML/ex8 Anomaly Detection and Recommender Systems/cofiCostFunc.m
+++ ML/ex8 Anomaly Detection and Recommender Systems/cofiCostFunc.m
@@ -41,15 +41,14 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+J = (1/2)*sum(sum((((X*Theta')-Y).*R).^2));
+X_grad = (((X*Theta')-Y).*R)*Theta;
+Theta_grad = (((X*Theta')-Y).*R)'*X;
 
 
-
-
-
-
-
-
-
+J = J +((lambda/2)*sum(sum(Theta.^2)))+((lambda/2)*sum(sum(X.^2)));
+X_grad = X_grad + (lambda*X);
+Theta_grad = Theta_grad + (lambda*Theta);
 
 
 
