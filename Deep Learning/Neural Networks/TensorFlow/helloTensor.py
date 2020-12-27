b3f0c36f93e9bedfc95ba83b36346c3e5d49d436
Bhupend Patil <bhupendpatil@gmail.com>
2020-12-27 21:33:22 +0530

    Hello, Bhupend!

--- Deep Learning/Neural Networks/TensorFlow/helloTensor.py
+++ Deep Learning/Neural Networks/TensorFlow/helloTensor.py
@@ -1,9 +1,21 @@
 import tensorflow as tf
 
+# one way
 hello_constant = tf.constant("Hello World!")
 tf.print(hello_constant)
 
 """
 Output:
 Hello World!
+"""
+
+# another way
+tf.compat.v1.disable_eager_execution()
+hello = tf.constant("Hello, Bhupend!")
+sess = tf.compat.v1.Session()
+print(sess.run(hello))
+
+"""
+Output:
+b'Hello, Bhupend!'
 """
