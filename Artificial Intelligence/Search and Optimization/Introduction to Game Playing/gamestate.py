43d52eca1eee88f60e2f4828752bf9df5848550f
Bhupend Patil <bhupendpatil@gmail.com>
2021-02-05 13:40:12 +0530

    Game Class Functionality

--- Artificial Intelligence/Search and Optimization/Introduction to Game Playing/gamestate.py
+++ Artificial Intelligence/Search and Optimization/Introduction to Game Playing/gamestate.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 xlim,ylim=3,2
 class GameState:
     def __init__(self):
@@ -5,6 +7,52 @@ class GameState:
         self._board[-1][-1] = 1
         self._parity = 0
         self._player_locations = [None,None]
+        
+    def get_legal_moves(self):
+        """ Return a list of all legal moves available to the
+        active player.  Each player should get a list of all
+        empty spaces on the board on their first move, and
+        otherwise they should get a list of all open spaces
+        in a straight line along any row, column or diagonal
+        from their current position. (Players CANNOT move
+        through obstacles or blocked squares.) Moves should
+        be a pair of integers in (column, row) order specifying
+        the zero-indexed coordinates on the board.
+        """
+        # TODO: implement this function!
+        loc = self._player_locations[self._parity]
+        if not loc:
+            return [(x,y) for y in range(ylim) for x in range(xlim) if self._board[x][y]==0]
+        moves = []
+        rays = [(1, 0), (1, -1), (0, -1), (-1, -1),(-1, 0), (-1, 1), (0, 1), (1, 1)]
+        for dx,dy in rays:
+            _x,_y = loc
+            while 0 <= _x+dx < xlim and 0 <= _y+dx < ylim:
+                _x,_y = _x+dx, _y+dy
+                if self._board[_x][_y]:
+                    break
+                moves.append((_x,_y))
+        return moves
+
+    def forecast_move(self, move):
+        """ Return a new board object with the specified move
+        applied to the current game state.
+        
+        Parameters
+        ----------
+        move: tuple
+            The target position for the active player's next move
+            (e.g., (0, 0) if the active player will move to the
+            top-left corner of the board)
+        """
+        # TODO: implement this function!
+        if move not in self.get_legal_moves():
+            raise RuntimeError("Illegal move attempt")
+        newBoard = deepcopy(self)
+        newBoard._board[move[0]][move[1]]=1
+        newBoard._player_locations[self._parity]
+        newBoard._parity ^= 1
+        return newBoard
 
 if __name__ == "__main__":
     emptyState = GameState()
--- Artificial Intelligence/Search and Optimization/Introduction to Game Playing/testcode.py
+++ Artificial Intelligence/Search and Optimization/Introduction to Game Playing/testcode.py
@@ -1,5 +1,30 @@
 from gamestate import *
 
 print("Creating empty game board...")
-emptyState = GameState()
-print("Everything looks good!")
+g = GameState()
+
+print("Getting legal moves for player 1...")
+p1_empty_moves = g.get_legal_moves()
+print("Found {} legal moves.".format(len(p1_empty_moves or [])))
+
+print("Applying move (0, 0) for player 1...")
+g1 = g.forecast_move((0, 0))
+
+print("Getting legal moves for player 2...")
+p2_empty_moves = g1.get_legal_moves()
+if (0, 0) in set(p2_empty_moves):
+    print("Failed\n  Uh oh! (0, 0) was not blocked properly when " +
+          "player 1 moved there.")
+else:
+    print("Everything looks good!")
+
+"""
+Output:
+
+Creating empty game board...
+Getting legal moves for player 1...
+Found 5 legal moves.
+Applying move (0, 0) for player 1...
+Getting legal moves for player 2...
+Everything looks good!
+"""
