e8c1b63a260914971d6f1432b1f1b300ad309fd9
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-04 01:31:09 +0530

    to components dir

--- React/arcdevelopment/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Hello!</div>;
-}
-
-export default App;
---
+++ React/arcdevelopment/src/components/App.js
@@ -0,0 +1,12 @@
+import Header from "../components/ui/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hello!
+    </div>
+  );
+}
+
+export default App;
