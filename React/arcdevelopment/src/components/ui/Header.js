15e33c90770543bb56b88a9142c3a43e67dd0073
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-04 01:30:50 +0530

    AppBar Component

---
+++ React/arcdevelopment/src/components/ui/Header.js
@@ -0,0 +1,10 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+export default function Header(props) {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+}
