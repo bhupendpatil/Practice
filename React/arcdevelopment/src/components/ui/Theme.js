f0b0827862b5bb782b7f44976eb2b72b186408cb
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-05 23:31:12 +0530

    styling
    palette

--- React/arcdevelopment/src/components/ui/Theme.js
+++ React/arcdevelopment/src/components/ui/Theme.js
@@ -1,3 +1,19 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export default createMuiTheme({});
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+});
