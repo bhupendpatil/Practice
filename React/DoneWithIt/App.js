0fa7097c5a6740dfccea3385ea439605997e5243
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-13 20:46:42 +0530

    touchables
    Pressable

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -1,19 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, Image } from "react-native";
+import { SafeAreaView, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello Bhupend!</Text>
-      <Image
-        blurRadius={1}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <Pressable onPress={() => console.log("Pressable Pressed")}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </Pressable>
     </SafeAreaView>
   );
 }
