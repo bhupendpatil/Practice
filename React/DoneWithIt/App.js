e75c5a17e27cebdc5d5b578faeafc369849b84e4
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-13 20:22:53 +0530

    text

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -3,11 +3,14 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  console.log("App Executed");
+  const handelPress = () => console.log("Handel Pressed");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Bhupend!</Text>
+      <Text numberOfLines={1} onPress={handelPress}>
+        Hello duehuehudhe hdu ueh due huheu eu ue ueueu ue ue ueueu eu ue ue hue
+        ueueu uehudpeojeofp
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
