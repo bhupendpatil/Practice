0d8d481b289a310e06b029533c6b46186014b342
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-18 14:05:37 +0530

    dimensions

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import {
-  Button,
+  Dimensions,
   SafeAreaView,
-  Alert,
   StyleSheet,
   Platform,
   StatusBar,
+  View,
 } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-        color="#fff"
-        title="Click me"
-        onPress={() =>
-          Alert.prompt("My title", "My message", (text) => console.log(text))
-        }
-      />
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "50%",
+          height: 70,
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
