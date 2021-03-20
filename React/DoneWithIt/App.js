2c5798883ef3e2a8465b9e1ef4b527dc9098fd8b
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-20 13:08:35 +0530

    Device Orientation

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -6,16 +6,17 @@ import {
   StatusBar,
   View,
 } from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%%",
-          height: "30%",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
         }}
       ></View>
     </SafeAreaView>
