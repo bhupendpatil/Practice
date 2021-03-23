143d955e10a44cbcd82b63b88dc08126ba5e5bb3
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-23 13:52:59 +0530

    welcome screen

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -1,60 +1,6 @@
 import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Platform,
-  StatusBar,
-  View,
-} from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          alignContent: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-            left: 20,
-            top: 20,
-            position: "relative",
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-      </View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
