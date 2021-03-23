d3ddcd74d090815e7f56f7b99a1d69c91e3ac439
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-23 14:09:35 +0530

    refactor

--- React/DoneWithIt/app/screens/ViewImageScreen.js
+++ React/DoneWithIt/app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
+import colors from "../config/colors";
+
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
@@ -17,13 +19,13 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#000",
+    backgroundColor: colors.black,
     flex: 1,
   },
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 15,
     left: 30,
@@ -31,7 +33,7 @@ const styles = StyleSheet.create({
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
     position: "absolute",
     top: 15,
     right: 30,
