2f1e94c5a9b1d6e493ac8ab82fd34cd5a7ff7781
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-23 14:05:40 +0530

    building view image screen

--- React/DoneWithIt/App.js
+++ React/DoneWithIt/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
---
+++ React/DoneWithIt/app/screens/ViewImageScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 15,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 15,
+    right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
