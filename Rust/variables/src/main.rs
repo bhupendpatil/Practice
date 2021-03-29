1898574209a1922355a6b98f1fbf1b63c8648f52
Bhupend Patil <bhupendpatil@gmail.com>
2021-03-29 20:51:47 +0530

    rust variables

---
+++ Rust/variables/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // variables
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // constant
+    const MAX_POINTS: u32 = 100_000;
+    println!("{}", MAX_POINTS);
+
+    // shadowing a variable
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces: {}", spaces);
+}
