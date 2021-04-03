4af88bd5ff9201e1f73e119918d2ece74d9d2372
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-03 19:14:17 +0530

    using vue

--- Vue.js/starting-project/app.js
+++ Vue.js/starting-project/app.js
@@ -1,3 +1,18 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
+
 // const buttonEl = document.querySelector("button");
 // const inputEl = document.querySelector("input");
 // const listEl = document.querySelector("ul");
