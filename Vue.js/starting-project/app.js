abb2efd03fa7b37c1f29dd0978fe7677751fbeee
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-03 15:16:01 +0530

    //

--- Vue.js/starting-project/app.js
+++ Vue.js/starting-project/app.js
@@ -1,13 +1,13 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = "";
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = "";
+// }
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
