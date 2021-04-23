825367e5254fd183b978d9db19848be899ab301c
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-23 15:19:15 +0530

    Understanding methods in Vue Apps

--- Vue.js/basics/app.js
+++ Vue.js/basics/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       vueLink: "https://bhupend.com",
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
