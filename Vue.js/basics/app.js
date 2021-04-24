46a8a80801d8415d9fbfc3d428f22a2cc4748488
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-25 00:30:55 +0530

    Understanding Event Binding

--- Vue.js/basics/app.js
+++ Vue.js/basics/app.js
@@ -1,21 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: "Finish the course and learn Vue!",
-      courseGoalB: "Master Vue and build amazing apps",
-      vueLink: "https://bhupend.com",
+      counter: 0,
     };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    },
   },
 });
 
-app.mount("#user-goal");
+app.mount('#events');
