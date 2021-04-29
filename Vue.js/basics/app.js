ab63e9522158b169ac5c18ca3e6b7749b60d2036
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-29 13:56:42 +0530

    Dynamic Styling with Inline Styles

--- Vue.js/basics/app.js
+++ Vue.js/basics/app.js
@@ -1,68 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: "",
-      lastName: "",
-      fullName: "",
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
-  computed: {
-    // fullName() {
-    //   if (this.name === "") {
-    //     return "";
-    //   } else {
-    //     return this.name + " " + "Patil";
-    //   }
-    // },
-  },
-  watch: {
-    counter(value) {
-      if (value > 50) {
-        const that = this;
-        setTimeout(function () {
-          that.counter = 0;
-        }, 2000);
-      }
-    },
-    name(value) {
-      if (value === "") {
-        this.fullName = "";
-      } else {
-        this.fullName = value + " " + "Patil";
-      }
-    },
-    lastName(value) {
-      if (value === "") {
-        this.fullName = "";
-      } else {
-        this.fullName = "Bhupend" + " " + value;
-      }
-    },
-  },
   methods: {
-    outputFullname() {
-      if (this.name === "") {
-        return "";
-      } else {
-        return this.name + " " + "Patil";
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = true;
+      } else if (box === "B") {
+        this.boxBSelected = true;
+      } else if (box === "C") {
+        this.boxCSelected = true;
       }
-    },
-    setName(event) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput() {
-      this.name = "";
-      this.lastName = "";
     },
   },
 });
 
-app.mount("#events");
+app.mount("#styling");
