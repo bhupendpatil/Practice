acaaa61ac2f04d3a73b9b8d26f73dcf4e62c1f29
Bhupend Patil <bhupendpatil@gmail.com>
2021-04-29 14:12:00 +0530

    Classes & Computed Properties

--- Vue.js/basics/app.js
+++ Vue.js/basics/app.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box === "A") {
