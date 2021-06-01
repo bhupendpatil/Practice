e1573171a59f0739083589a3e0d74cf5090e5bc3
Bhupend Patil <bhupendpatil@gmail.com>
2021-06-01 13:11:17 +0530

    multi instances

--- Vue.js/fundamentals/app.js
+++ Vue.js/fundamentals/app.js
@@ -1,1 +1,17 @@
-Vue.createApp({}).mount("#app");
+Vue.createApp({
+  data() {
+    return {
+      firstName: "Bhupend",
+      lastName: "Patil",
+    };
+  },
+}).mount("#app");
+
+Vue.createApp({
+  data() {
+    return {
+      firstName: "Bhupend",
+      lastName: "Patil",
+    };
+  },
+}).mount("#app1");
