d77cfc53843bbe304d404ac6299e78c20cd87b8b
Bhupend Patil <bhupendpatil@gmail.com>
2021-06-29 13:57:23 +0530

    Creating a Perspective button

---
+++ Vue.js/Project1PerspectivePlayground/app.js
@@ -0,0 +1,30 @@
+Vue.createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    };
+  },
+  computed: {
+    box() {
+      return {
+        transform: `
+            perspective(${this.perspective}px)
+            rotateX(${this.rotateX}deg)
+            rotateY(${this.rotateY}deg)
+            rotateZ(${this.rotateZ}deg)
+        `,
+      };
+    },
+  },
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+  },
+}).mount("#app");
