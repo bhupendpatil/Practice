e7b7abed5b91fb7850a874160cc12ede93c91a6d
Bhupend Patil <bhupendpatil@gmail.com>
2021-06-29 14:08:02 +0530

    Copying to the Clipboard

--- Vue.js/Project1PerspectivePlayground/app.js
+++ Vue.js/Project1PerspectivePlayground/app.js
@@ -26,5 +26,18 @@ Vue.createApp({
       this.rotateY = 0;
       this.rotateZ = 0;
     },
+    copy() {
+      const el = document.createElement("textarea");
+
+      el.setAttribute("readonly", "");
+      el.style.position = "absolute";
+      el.style.left = "-9999px";
+      el.value = `transform: ${this.box.transform}`;
+
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand("copy");
+      document.body.removeChild(el);
+    },
   },
 }).mount("#app");
