ac4bcedf94597b178d4bff98df2d44dca5315486
Bhupend Patil <bhupendpatil@gmail.com>
2021-06-30 12:15:49 +0530

    Mounting the Vue Instance

--- Vue.js/beyondTheFundamentals/app.js
+++ Vue.js/beyondTheFundamentals/app.js
@@ -1,7 +1,13 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
-  }
-}).mount('#app')
+      message: "Hello world!",
+    };
+  },
+});
+
+vm.mount("#app");
+
+// setTimeout(() => {
+//   vm.mount("#app");
+// }, 3000);
