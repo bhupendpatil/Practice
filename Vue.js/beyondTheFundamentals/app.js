5368959105bcc0a6842ad7b3cb69f98a59c8a882
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-03 17:11:20 +0530

    using lifecycle hooks

--- Vue.js/beyondTheFundamentals/app.js
+++ Vue.js/beyondTheFundamentals/app.js
@@ -4,6 +4,30 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
+  beforeCreate() {
+    console.log("beforeCreate() function called!", this.message);
+  },
+  created() {
+    console.log("created() function called!", this.message);
+  },
+  beforeMount() {
+    console.log("beforeMount() function called!", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function called!", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate() function called!");
+  },
+  updated() {
+    console.log("updated() function called!");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount() function called!");
+  },
+  unmounted() {
+    console.log("unmounted() function called!");
+  },
 });
 
 vm.mount("#app");
