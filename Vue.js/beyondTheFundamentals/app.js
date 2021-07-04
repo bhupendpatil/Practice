8cc9ae2f00922e192d8eecaaf59419013f37f166
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-04 20:45:59 +0530

    template

--- Vue.js/beyondTheFundamentals/app.js
+++ Vue.js/beyondTheFundamentals/app.js
@@ -35,3 +35,14 @@ vm.mount("#app");
 // setTimeout(() => {
 //   vm.mount("#app");
 // }, 3000);
+
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!",
+//     };
+//   },
+//   render() {
+//     return Vue.h("h1", this.message);
+//   },
+// }).mount("#app2");
