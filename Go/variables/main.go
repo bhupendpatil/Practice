e30a517ad66fadafb2da87274c5c1abb2a80d3ab
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-09 12:20:15 +0530

    var keyword

---
+++ Go/variables/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var b = 32
+var c int
+
+func main() {
+	// Short hand
+	x := 42
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + x
+	fmt.Println(y)
+	firstName := "Bhupend"
+	lastName := "Patil"
+	fmt.Println(firstName, lastName)
+
+	// var keyword
+	var a = 22
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+}
