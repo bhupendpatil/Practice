686a0210ec951b4ea0270d42c1a6a0235babc8e1
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-18 00:25:28 +0530

    Props

--- Vue.js/components/src/App.vue
+++ Vue.js/components/src/App.vue
@@ -1,15 +1,23 @@
 <template>
-<p>Hey!</p>
+<h3>Hey!</h3>
 <greeting />
+<user :age="age" />
 </template>
 
 <script>
 import Greeting from "./components/Greeting.vue";
+import User from "./components/User.vue";
 
 export default {
     name: "App",
     components: {
         Greeting,
+        User
+    },
+    data() {
+        return {
+            age: 20
+        }
     }
 }
 </script>
---
+++ Vue.js/components/src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+<p>The user is {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+    name: "User",
+    props: ["age"]
+}
+</script>
+
+<style>
+
+</style>
