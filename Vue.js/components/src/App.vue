aa57aa7e32be1ca52328138018d50705ea362900
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-17 14:15:43 +0530

    Creating Components

--- Vue.js/components/src/App.vue
+++ Vue.js/components/src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Hello</h1>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>{{ msg }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data(){
+    return {
+      msg: "Hello World!"
+    }
   }
-} 
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
