78c4a1db0371a7d6cbe9ef9e6b8a3abc1ef725af
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-19 15:00:55 +0530

    Named Slots

--- Vue.js/slots/src/App.vue
+++ Vue.js/slots/src/App.vue
@@ -1,18 +1,30 @@
 <template>
 <app-form>
-    <div class="help">
-        <p>This is some help text.</p>
-    </div>
-    <div class="fields">
+    <template v-slot:help>
+        <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
         <input type="text" placeholder="email">
         <input type="text" placeholder="username">
         <input type="password" placeholder="password">
-    </div>
-    <div class="buttons">
+    </template>
+    <template v-slot:buttons>
         <button type="submit">Submit</button>
-    </div>
+    </template>
+    <p>Dummy text</p>
 </app-form>
-<app-form></app-form>
+<app-form>
+    <template v-slot:help>
+        <p>Contact help text.</p>
+    </template>
+    <template v-slot:fields>
+        <input type="text" placeholder="name">
+        <input type="text" placeholder="message">
+    </template>
+    <template v-slot:buttons>
+        <button type="submit">Submit</button>
+    </template>
+</app-form>
 </template>
 
 <script>
@@ -22,6 +34,11 @@ export default {
     name: 'App',
     components: {
         AppForm,
+    },
+    data() {
+        return {
+            help: "This is some help text."
+        }
     }
 }
 </script>
--- Vue.js/slots/src/components/Form.vue
+++ Vue.js/slots/src/components/Form.vue
@@ -1,5 +1,14 @@
 <template>
 <form>
-    <slot>No form to render</slot>
+    <div class="help">
+        <slot name="help"></slot>
+    </div>
+    <div class="fields">
+        <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+        <slot name="buttons"></slot>
+    </div>
+    <slot></slot>
 </form>
 </template>
