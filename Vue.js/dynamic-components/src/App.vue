4bca091c79559ef3594532c4698fed924f40ca16
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-19 21:01:24 +0530

    Dynamic Components

--- Vue.js/dynamic-components/src/App.vue
+++ Vue.js/dynamic-components/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+</select>
+
+<keep-alive>
+    <component :is="componentName"></component>
+</keep-alive>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue"
+import About from "./components/About.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Home,
+        About
+    },
+    data() {
+        return {
+            componentName: "Home",
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ Vue.js/dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+    unmounted() {
+        console.log("Home component unmounted");
+    },
+    activated() {
+        console.log("Home component activated");
+    },
+    deactivated() {
+        console.log("Home component deactivated");
+    }
+}
+</script>
