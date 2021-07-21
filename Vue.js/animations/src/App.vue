34a9c140d03cbb702291d23289d670fbc305e483
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-21 14:25:58 +0530

    Animating with CSS Animations

--- Vue.js/animations/src/App.vue
+++ Vue.js/animations/src/App.vue
@@ -1,9 +1,13 @@
 <template>
 <button type="button" @click="flag = !flag">Toggle</button>
 
-<transition name="fade" mode="out-in">
+<!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello World!</h2>
     <h2 v-else key="secondary">Another hello!</h2>
+</transition> -->
+
+<transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
 </transition>
 </template>
 
@@ -12,7 +16,7 @@ export default {
     name: "App",
     data() {
         return {
-            flag: false,
+            flag: true,
         };
     },
 };
@@ -24,11 +28,49 @@ export default {
 }
 
 .fade-enter-active {
-    transition: all 0.25s linear;
+    transition: all 1s linear;
 }
 
 .fade-leave-to {
-    transition: all 3s linear;
+    transition: all 1s linear;
     opacity: 0;
+}
+
+.zoom-enter-active {
+    animation: zoom-in 1s linear forwards;
+    transition: all 2s linear;
+}
+
+.zoom-leave-active {
+    animation: zoom-out 1s linear backwards;
+    transition: all 2s linear;
+}
+
+.zoom-enter-from {
+    opacity: 0;
+}
+
+.zoom-leave-to {
+    opacity: 0;
+}
+
+@keyframes zoom-in {
+    from {
+        transform: scale(0, 0);
+    }
+
+    to {
+        transform: scale(1, 1);
+    }
+}
+
+@keyframes zoom-out {
+    from {
+        transform: scale(1, 1);
+    }
+
+    to {
+        transform: scale(0, 0);
+    }
 }
 </style>
