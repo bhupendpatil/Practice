f9999cf76672ca34d0d3720f5263ec2823e791eb
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-22 12:24:16 +0530

    Animating a list

--- Vue.js/animations/src/App.vue
+++ Vue.js/animations/src/App.vue
@@ -10,9 +10,16 @@
     <h2 v-if="flag">Hello</h2>
 </transition> -->
 
-<transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" :css="true" name="fade">
+<!-- <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" :css="true" name="fade">
     <h2 v-if="flag">Hello</h2>
-</transition>
+</transition> -->
+<button @click="addItem">Add</button>
+
+<ul>
+    <transition-group name="fade">
+        <li v-for="(number,index) in numbers" :key="number" @click="removeItem(index)">{{number}}</li>
+    </transition-group>
+</ul>
 </template>
 
 <script>
@@ -21,9 +28,18 @@ export default {
     data() {
         return {
             flag: true,
+            numbers: [1, 2, 3, 4, 5],
         };
     },
     methods: {
+        addItem() {
+            const num = Math.floor(Math.random() * 100 + 1);
+            const index = Math.floor(Math.random() * this.numbers.length);
+            this.numbers.splice(index, 0, num);
+        },
+        removeItem(index) {
+            this.numbers.splice(index, 1);
+        },
         beforeEnter(el) {
             console.log("before-enter event fired", el);
         },
@@ -67,6 +83,11 @@ export default {
 </script>
 
 <style>
+li {
+    font-size: 22px;
+    cursor: pointer;
+}
+
 .fade-enter-from {
     opacity: 0;
 }
