18812e84fa800a1c06d67a11b244059a56e9650e
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-22 12:40:05 +0530

    Fixing the animation

--- Vue.js/animations/src/App.vue
+++ Vue.js/animations/src/App.vue
@@ -101,6 +101,14 @@ li {
     opacity: 0;
 }
 
+.fade-move {
+    transition: all 1s linear;
+}
+
+.fade-leave-active {
+    position: absolute;
+}
+
 .zoom-enter-active {
     animation: zoom-in 1s linear forwards;
     transition: all 2s linear;
