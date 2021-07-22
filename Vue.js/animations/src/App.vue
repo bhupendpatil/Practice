6a7c6ca4886de71f55a2d9fc799b41e2e910170d
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-22 12:51:16 +0530

    Transition CSS class names

--- Vue.js/animations/src/App.vue
+++ Vue.js/animations/src/App.vue
@@ -16,7 +16,7 @@
 <button @click="addItem">Add</button>
 
 <ul>
-    <transition-group name="fade">
+    <transition-group name="fade" enter-active-class="animate__animated animate__flipInX" leave-active-class="animate__animated animate__flipOutX">
         <li v-for="(number,index) in numbers" :key="number" @click="removeItem(index)">{{number}}</li>
     </transition-group>
 </ul>
@@ -83,6 +83,14 @@ export default {
 </script>
 
 <style>
+.animate__flipOutX {
+    position: absolute;
+}
+
+.animate__animated {
+    animation-duration: 1.5s;
+}
+
 li {
     font-size: 22px;
     cursor: pointer;
