ee0189efb6906df59ff012edefbc9b64a50404ba
Bhupend Patil <bhupendpatil@gmail.com>
2021-07-27 14:56:59 +0530

    Moving between Questions

--- Vue.js/quiz/src/App.vue
+++ Vue.js/quiz/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length" :questions="questions" />
+    <questions v-if="questionsAnswered < questions.length" :questions="questions" :questionsAnswered="questionsAnswered" @question-answered="questionAnswered" />
     <result v-else />
 
     <button type="button" class="reset-btn">Reset</button>
@@ -20,6 +20,7 @@ export default {
     data() {
         return {
             questionsAnswered: 0,
+            totalCorrect: 0,
             questions: [{
                     q: 'What is 2 + 2?',
                     answers: [{
@@ -90,6 +91,14 @@ export default {
                     desc: "Studying has definitely paid off for you!"
                 }
             ]
+        }
+    },
+    methods: {
+        questionAnswered(is_correct) {
+            if (is_correct) {
+                this.totalCorrect++;
+            }
+            this.questionsAnswered++;
         }
     }
 };
--- Vue.js/quiz/src/components/Questions.vue
+++ Vue.js/quiz/src/components/Questions.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="questions-ctr">
     <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
+        <div class="bar" :style="{width:`${questionsAnswered / questions.length * 100}%`}"></div>
+        <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
     </div>
-    <div class="single-question" v-for="question in questions" :key="question.q">
+    <div class="single-question" v-for="(question,qi) in questions" :key="question.q" v-show="questionsAnswered===qi">
         <div class="question">{{question.q}}</div>
         <div class="answers">
-            <div class="answer" v-for="answer in question.answers" :key="answer.text">{{ answer.text }}</div>
+            <div class="answer" v-for="answer in question.answers" :key="answer.text" @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
         </div>
     </div>
 </div>
@@ -15,6 +15,12 @@
 
 <script>
 export default {
-    props: ["questions"],
+    props: ["questions", "questionsAnswered"],
+    emits: ["question-answered"],
+    methods: {
+        selectAnswer(is_correct) {
+            this.$emit("question-answered", is_correct)
+        }
+    }
 }
 </script>
