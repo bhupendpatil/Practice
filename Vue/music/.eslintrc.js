cbd8f12d45bcccdf553761963882814868dc1bad
Bhupend Patil <bhupendpatil@gmail.com>
2021-08-25 14:23:20 +0530

    reconfigured

--- Vue/music/.eslintrc.js
+++ Vue/music/.eslintrc.js
@@ -3,18 +3,23 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': 0,
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
       env: {
         jest: true,
       },
