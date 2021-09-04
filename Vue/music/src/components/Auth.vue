817da11c1c395f54dc72babce89daf31157a4850
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-04 14:05:18 +0530

    Adding Tabs

--- Vue/music/src/components/Auth.vue
+++ Vue/music/src/components/Auth.vue
@@ -27,16 +27,22 @@
                 <!-- Tabs -->
                 <ul class="flex flex-wrap mb-4">
                     <li class="flex-auto text-center">
-                        <a class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600" href="#">Login</a>
+                        <a class="block rounded py-3 px-4 transition" href="#" @click.prevent="tab = 'login'"
+                        :class="{
+                            'hover:text-white text-white bg-blue-600': tab ==='login',
+                            'hover:text-blue-600': tab === 'register'
+                            }">Login</a>
                     </li>
                     <li class="flex-auto text-center">
-                        <a class="block rounded py-3 px-4 transition" href="#">Register</a>
+                        <a class="block rounded py-3 px-4 transition" href="#" @click.prevent="tab = 'register'" :class="{
+                            'hover:text-white text-white bg-blue-600': tab ==='register',
+                            'hover:text-blue-600': tab === 'login'
+                            }">Register</a>
                     </li>
                 </ul>
 
                 <!-- Login Form -->
-                <form>
+                <form v-show="tab==='login'">
                     <!-- Email -->
                     <div class="mb-3">
                         <label class="inline-block mb-2">Email</label>
@@ -55,7 +61,7 @@
                     </button>
                 </form>
                 <!-- Registration Form -->
-                <form>
+                <form v-show="tab==='register'">
                     <!-- Name -->
                     <div class="mb-3">
                         <label class="inline-block mb-2">Name</label>
@@ -117,6 +123,11 @@ import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'Auth',
+  data() {
+    return {
+      tab: 'login',
+    };
+  },
   computed: {
     // ...mapState({
     //   modal: 'authModalShow',
