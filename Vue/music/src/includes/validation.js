4193a64f14768399e9baaf441ded8ad487b11391
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-05 12:07:15 +0530

    Defining Rules

--- Vue/music/src/includes/validation.js
+++ Vue/music/src/includes/validation.js
@@ -1,8 +1,11 @@
-import { Form as VeeForm, Field as VeeField } from 'vee-validate';
+import { Form as VeeForm, Field as VeeField, defineRule } from 'vee-validate';
+import { required } from '@vee-validate/rules';
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
+
+    defineRule('required', required);
   },
 };
