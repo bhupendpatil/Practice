f625c1876d7b5cf1bc9b526cc4c20927a2d953d6
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-07 14:30:49 +0530

    Rendering Multiple Error Messages

--- Vue/music/src/components/Auth.vue
+++ Vue/music/src/components/Auth.vue
@@ -86,8 +86,13 @@
                     <!-- Password -->
                     <div class="mb-3">
                         <label class="inline-block mb-2">Password</label>
-                        <vee-field type="password" name="password" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded" placeholder="Password" />
+                        <vee-field name="password" :bails="false" v-slot="{ field,errors }">
+                          <input type="password" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                  duration-500 focus:outline-none focus:border-black rounded" placeholder="Password" v-bind="field"/>
+                          <div class="text-red-600" v-for="error in errors" :key="error">
+                              {{error}}
+                          </div>
+                        </vee-field>
                     <ErrorMessage class="text-red-600" name="password"/>
                     </div>
                     <!-- Confirm Password -->
