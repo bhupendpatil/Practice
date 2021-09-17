08de8b203c2d97fc8891fbd144a543eea3145658
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-17 14:03:53 +0530

    Custom Error Messages

--- Vue/music/src/components/Auth.vue
+++ Vue/music/src/components/Auth.vue
@@ -144,9 +144,9 @@ export default {
         email: 'required|min:3|max:100|email',
         age: 'required|min_value:18|max_value:100',
         password: 'required|min:3|max:100',
-        confirm_password: 'confirmed:@password',
-        country: 'required|excluded:Antartica',
-        tos: 'required',
+        confirm_password: 'passwords_mismatch:@password',
+        country: 'required|country_excluded:Antartica',
+        tos: 'tos',
       },
       userData: {
         country: 'USA',
--- Vue/music/src/includes/validation.js
+++ Vue/music/src/includes/validation.js
@@ -1,5 +1,5 @@
 import {
-  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage,
+  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure,
 } from 'vee-validate';
 import {
   required, min, max, alpha_spaces as alphaSpaces, email,
@@ -14,13 +14,37 @@ export default {
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alpha_spaces', alphaSpaces);
     defineRule('email', email);
     defineRule('min_value', minValue);
     defineRule('max_value', maxValue);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces`,
+          email: `The field ${ctx.field} must be a valid email`,
+          min_value: `The field ${ctx.field} is too low`,
+          max_value: `The field ${ctx.field} is too high`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}`,
+          country_excluded: 'Due to restriction, we do not accept users from this location',
+          passwords_mismatch: "The passwords don't match",
+          tos: 'You must accept the Terms of Service',
+        };
+
+        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid`;
+
+        return message;
+      },
+    });
   },
 };
