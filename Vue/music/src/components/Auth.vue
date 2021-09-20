36125252d73440cba53cf3aa3dbbbad6caa6a02a
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-20 14:25:36 +0530

    Showing Alerts

--- Vue/music/src/components/Auth.vue
+++ Vue/music/src/components/Auth.vue
@@ -61,6 +61,9 @@
                     </button>
                 </form>
                 <!-- Registration Form -->
+                <div class="text-white text-center font-bold p-5 mb-4" v-if="reg_show_alert" :class="reg_alert_variant">
+                  {{ reg_alert_msg }}
+                </div>
                 <vee-form v-show="tab==='register'" :validation-schema="schema" @submit="register" :initial-values="userData">
                     <!-- Name -->
                     <div class="mb-3">
@@ -120,7 +123,7 @@
                         <label class="inline-block">Accept terms of service</label>
                     <ErrorMessage class="text-red-600" name="tos"/>
                     </div>
-                    <button type="submit" class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
+                    <button type="submit" :disabled="reg_in_submission" class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700">
                         Submit
                     </button>
@@ -151,6 +154,10 @@ export default {
       userData: {
         country: 'USA',
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created',
     };
   },
   computed: {
@@ -162,6 +169,13 @@ export default {
   methods: {
     ...mapMutations(['toggleAuthModal']),
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = 'bg-blue-500';
+      this.reg_show_alert = 'Please wait! Your account is being created';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg = 'Success! Your account has been created';
       console.log(values);
     },
   },
