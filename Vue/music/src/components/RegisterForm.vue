fc88744054c5e258baeb41396d34e7fcafd3a083
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-29 00:48:59 +0530

    Handling the Response

--- Vue/music/src/components/RegisterForm.vue
+++ Vue/music/src/components/RegisterForm.vue
@@ -102,13 +102,21 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_show_alert = 'Please wait! Your account is being created';
 
-      const userCred = await firebase.auth().createUserWithEmailAndPassword(
-        values.email, values.password,
-      );
+      let userCred = null;
+      try {
+        userCred = await firebase.auth().createUserWithEmailAndPassword(
+          values.email, values.password,
+        );
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later';
+        return;
+      }
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
-      console.log(values);
+      console.log(userCred);
     },
   },
 };
