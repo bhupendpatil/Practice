8d3c9c487dd2e19343d9ad2038f274e7ce19d335
Bhupend Patil <bhupendpatil@gmail.com>
2021-09-30 15:07:49 +0530

    Connecting the User with their Data

--- Vue/music/src/components/RegisterForm.vue
+++ Vue/music/src/components/RegisterForm.vue
@@ -70,8 +70,6 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
-
 export default {
   name: 'RegisterForm',
   data() {
@@ -102,37 +100,17 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_show_alert = 'Please wait! Your account is being created';
 
-      let userCred = null;
       try {
-        userCred = await auth.createUserWithEmailAndPassword(
-          values.email, values.password,
-        );
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
         this.reg_alert_msg = 'An unexpected error occured. Please try again later';
         return;
       }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'An unexpected error occured. Please try again later';
-        return;
-      }
-
-      this.$store.commit('toggleAuth');
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
-      console.log(userCred);
     },
   },
 };
--- Vue/music/src/store/index.js
+++ Vue/music/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
@@ -15,5 +16,25 @@ export default createStore({
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+  },
+  actions: {
+    async register({ commit }, payload) {
+      const userCred = await auth.createUserWithEmailAndPassword(
+        payload.email, payload.password,
+      );
+
+      await usersCollection.doc(userCred.user.uid).set({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      await userCred.user.updateProfile({
+        displayName: payload.name,
+      });
+
+      commit('toggleAuth');
+    },
   },
 });
