8db6b2819410e78014ab320898ff5bfc40ea2269
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-01 13:24:10 +0530

    Creating Routers

--- Vue/music/src/router/index.js
+++ Vue/music/src/router/index.js
@@ -1,7 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
 const routes = [
-  
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
 ];
 
 const router = createRouter({
