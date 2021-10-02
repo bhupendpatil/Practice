bd2a97069f3252633fc0d0110798479e93184b3b
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-02 14:02:48 +0530

    Naming Routes

--- Vue/music/src/components/Header.vue
+++ Vue/music/src/components/Header.vue
@@ -4,21 +4,21 @@
 <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
         <!-- App Name -->
-        <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/" exact-active-class="no-active">Music</router-link>
+        <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{ name: 'home' }" exact-active-class="no-active">Music</router-link>
 
         <div class="flex flex-grow items-center">
             <!-- Primary Navigation -->
             <ul class="flex flex-row mt-1">
                 <!-- Navigation Links -->
                 <li>
-                    <router-link class="px-2 text-white" to="/about">About</router-link>
+                    <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
                 </li>
                 <li v-if="!userLoggedIn">
                     <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
                 </li>
                 <template v-else>
                 <li>
-                    <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+                    <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
                 </li>
                 <li>
                     <a class="px-2 text-white" href="#" @click.prevent="signout">Logout</a>
--- Vue/music/src/router/index.js
+++ Vue/music/src/router/index.js
@@ -5,14 +5,17 @@ import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
     component: About,
   },
   {
+    name: 'manage',
     path: '/manage',
     component: Manage,
   },
