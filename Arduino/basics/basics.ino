cc91c9e8a3f250226e230d556e6c7371133ec918
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-08 19:53:04 +0530

    getting started with custom functions

--- Arduino/basics/basics.ino
+++ Arduino/basics/basics.ino
@@ -1,12 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Hello");
-  Serial.print("hello again");
+  Serial.println("Simple Calculation using function");
+  do_a_calc(4,5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(millis());
-  delay(1000);
+  
+}
+
+int do_a_calc(int number_1, int number_2) {
+  Serial.println(number_1 + number_2);
 }
