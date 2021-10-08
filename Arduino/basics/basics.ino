6d7aa06dc227c5f7170691d47708f01fb341554f
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-08 23:33:53 +0530

    variables

--- Arduino/basics/basics.ino
+++ Arduino/basics/basics.ino
@@ -2,7 +2,11 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Simple Calculation using function");
-  Serial.println(do_a_calc(4,5));
+
+  int first_number = 5;
+  int second_number;
+  second_number = 9;
+  Serial.println(do_a_calc(first_number,second_number));
 }
 
 void loop() {
@@ -12,5 +16,6 @@ void loop() {
 
 int do_a_calc(int number_1, int number_2) {
   // Serial.println(number_1 + number_2);
-  return number_1 + number_2;
+  int result = number_1 + number_2;
+  return result;
 }
