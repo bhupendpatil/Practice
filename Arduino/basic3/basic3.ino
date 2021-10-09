e02013c1858bc8ce921e2fbc4f6aa593f665008a
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-09 13:39:07 +0530

    while

---
+++ Arduino/basic3/basic3.ino
@@ -0,0 +1,18 @@
+// while
+int counter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (counter < 10){
+    Serial.print(counter);
+    Serial.print(", ");
+    Serial.println("Counter is smaller than 10");
+    delay(500);
+    counter++;
+  }
+}
