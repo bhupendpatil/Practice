5097708015328eb58b609fc9b3ac97e62507f799
Bhupend Patil <bhupendpatil@gmail.com>
2021-10-09 13:25:30 +0530

    variable scope

--- Arduino/basics/basics.ino
+++ Arduino/basics/basics.ino
@@ -1,12 +1,14 @@
+int first_number = 2; // global variable
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Simple Calculation using function");
 
-  int first_number = 5;
+  // int first_number = 5;
   int second_number;
   second_number = 9;
-  Serial.println(do_a_calc(first_number,second_number));
+  Serial.println(do_a_calc(second_number));
 }
 
 void loop() {
@@ -14,8 +16,9 @@ void loop() {
   
 }
 
-int do_a_calc(int number_1, int number_2) {
+int do_a_calc(int number_2) {
   // Serial.println(number_1 + number_2);
-  int result = number_1 + number_2;
+  first_number = 10;
+  int result = first_number + number_2;
   return result;
 }
