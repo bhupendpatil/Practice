a4edc019d4cb6360f641d115864b7dde0e706b69
Bhupend Patil <bhupendpatil@gmail.com>
2021-11-01 16:56:15 +0530

    Working with Number, String and Boolean

--- TypeScript/understanding-TS/app.js
+++ TypeScript/understanding-TS/app.js
@@ -1,7 +1,17 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
