a91b915e92f96da7f03d2122e3dd42f06eaa752f
Bhupend Patil <bhupendpatil@gmail.com>
2021-11-02 17:54:24 +0530

    Object Types

--- TypeScript/understanding-TS/app.js
+++ TypeScript/understanding-TS/app.js
@@ -1,18 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('Incorrect input!');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-var result = add(number1, number2, printResult, resultPhrase);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: "Bhupend",
+    age: 26
+};
+console.log(person);
