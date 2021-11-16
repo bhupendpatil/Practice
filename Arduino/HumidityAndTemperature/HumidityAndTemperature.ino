7010fa99ba7f6408b013b46a9da59485461302b6
Bhupend Patil <bhupendpatil@gmail.com>
2021-11-16 08:59:56 +0530

    DHT11

---
+++ Arduino/HumidityAndTemperature/HumidityAndTemperature.ino
@@ -0,0 +1,25 @@
+// Library used https://github.com/adidax/dht11/zipball/master
+// Get Room Humidity and Temperature
+
+#include <dht11.h>
+#define DHT11PIN 4
+
+dht11 DHT11;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.println();
+  
+  DHT11.read(DHT11PIN);
+  
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+  
+  Serial.print("Temperature (C): ");
+  Serial.println((float)DHT11.temperature, 2);
+  
+  delay(2000);
+}
