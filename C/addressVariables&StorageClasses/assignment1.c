fe90080787e62c640641c4018342e76db7b042a4
Bhupend Patil <bhupendpatil@gmail.com>
2022-12-21 17:20:03 +0530

    spaces

--- C/addressVariables&StorageClasses/assignment1.c
+++ C/addressVariables&StorageClasses/assignment1.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Write a program to print the below texts using printf function
 
 David says, " Programming is fun !"
@@ -12,12 +12,12 @@ This is a triple quoted string """ This month has 30 days """
 #include <stdio.h>
 
 int main(){
-    printf(" David says, \" Programming is fun !\"\n");
-    printf(" **Conditions apply, \"Offers valid until tomorrow\"\n");
-    printf(" C:\\My computer\\My folder\n");
-    printf(" D:/My documents/My file\n");
-    printf(" \\ \\ \\ \\ Today is holiday \\ \\ \\ \\\n");
-    printf(" This is a triple quoted string \"\"\" This month has 30 days \"\"\"");
+    printf("David says, \" Programming is fun !\"\n");
+    printf("**Conditions apply, \"Offers valid until tomorrow\"\n");
+    printf("C:\\My computer\\My folder\n");
+    printf("D:/My documents/My file\n");
+    printf("\\ \\ \\ \\ Today is holiday \\ \\ \\ \\\n");
+    printf("This is a triple quoted string \"\"\" This month has 30 days \"\"\"");
 
     return 0;
 }
