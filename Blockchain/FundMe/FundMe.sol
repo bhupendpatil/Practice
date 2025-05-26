bf2efbcec4e465226450b01f2aad05bf8fbbe701
Bhupend Patil <bhupendpatil@gmail.com>
2025-05-26 23:59:37 +0530

    constructor, modifier

--- Blockchain/FundMe/FundMe.sol
+++ Blockchain/FundMe/FundMe.sol
@@ -11,13 +11,19 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -32,4 +38,10 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
+
 }
