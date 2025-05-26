3b0a3e5edaf51171ba67aec2981c03767df9ec3e
Bhupend Patil <bhupendpatil@gmail.com>
2025-05-27 00:34:44 +0530

    receive(), fallback()

--- Blockchain/FundMe/FundMe.sol
+++ Blockchain/FundMe/FundMe.sol
@@ -3,22 +3,24 @@ pragma solidity ^0.8.30;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor(){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -40,7 +42,8 @@ contract FundMe {
     }
 
     modifier onlyOwner(){
-        require(msg.sender == owner, "Sender is not owner!");
+        //require(msg.sender == i_owner, "Sender is not owner!");
+        if (msg.sender != i_owner) revert NotOwner();
         _;
     }
 
