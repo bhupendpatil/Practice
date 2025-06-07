0d79ac8d720cbc188920f4f32af3ceb5b4662021
Bhupend Patil <bhupendpatil@gmail.com>
2025-06-07 14:13:42 +0530

    refactoring the mock smart contracr

--- Blockchain/FoundryFundMe/script/HelperConfig.s.sol
+++ Blockchain/FoundryFundMe/script/HelperConfig.s.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
 
     struct NetworkConfig {
@@ -35,5 +36,14 @@ contract HelperConfig {
         return ethConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockV3Aggregator)
+        });
+        return anvilConfig;
+    }
 }
