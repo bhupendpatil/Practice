709c75228949e3a675f5c3cb95faaa5095d5cb6f
Bhupend Patil <bhupendpatil@gmail.com>
2025-06-25 03:55:16 +0530

    build

--- Blockchain/tsenderui/next.config.ts
+++ Blockchain/tsenderui/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  basePath: "",
+  assetPrefix: "./",
+  trailingSlash: true
 };
 
 export default nextConfig;
